5e84ea5c3fe0336413edb3da51dd1e07e0acc6b0
Charles N. Christensen <charles.n.chr@gmail.com>
2021-08-13 15:20:55 +0100

    Split install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,3 @@ ln -sfn ~/dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/dotfiles/.vimrc ~/.vimrc
 ln -sfn ~/dotfiles/.spacemacs ~/.spacemacs
 
-ln -sfn ~/dotfiles/.taskrc ~/.taskrc
-ln -s /Users/cc/0main/3.Diverse/IT/.timewarrior ~/.timewarrior
-ln -s /Users/cc/0main/3.Diverse/IT/.task ~/.task
---
+++ install_extra.sh
@@ -0,0 +1,4 @@
+ln -sfn ~/dotfiles/.taskrc ~/.taskrc
+ln -s /Users/cc/0main/3.Diverse/IT/.timewarrior ~/.timewarrior
+ln -s /Users/cc/0main/3.Diverse/IT/.task ~/.task
+
