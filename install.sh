c6225e393c3966713ca405640b346e775014d5b1
Charles N. Christensen <charles.n.chr@gmail.com>
2022-01-15 04:10:23 +0000

    Added dockerfile
    Former-commit-id: b7118b304bc7ec2aff7a53c7f4fa3b7714ee7f73

--- install.sh
+++ install.sh
@@ -5,25 +5,7 @@
 # - node
 # - python
 
-# autojump
-git clone git://github.com/wting/autojump.git ~/autojump
-cd ~/autojump
-./install.py
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
-
-# antigen download
-mkdir -p $HOME/tools
-git clone https://github.com/zsh-users/antigen.git $HOME/tools/antigen
-source ~/tools/antigen/antigen.zsh
-antigen update
-
+# git clone git@github.com:charlesnchr/dotfiles
 
 # symbolic links
 ln -sfn ~/dotfiles/.zshrc ~/.zshrc
@@ -32,7 +14,32 @@ ln -sfn ~/dotfiles/.vimrc ~/.vimrc
 ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sfn ~/dotfiles/.spacemacs ~/.spacemacs
 
-# Nvim
+# source
+zsh
+source ~/.zshrc
+
+# autojump
+git clone git://github.com/wting/autojump.git ~/autojump
+cd ~/autojump
+./install.py
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# antigen download
+mkdir -p $HOME/tools
+git clone https://github.com/zsh-users/antigen.git $HOME/tools/antigen
+source ~/tools/antigen/bin/antigen.zsh
+antigen update
+
+
+
+# vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 pip install -U pynvim
