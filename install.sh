ba4059c142112be5c0b5d7a7e7b8e8d093ee3fb4
Charles Christensen <charles.n.chr@gmail.com>
2022-01-26 19:01:41 +0000

    Completed i3 setup
    Former-commit-id: 080174153c98e9156b84ea7354f57e89b33bebd7

--- install.sh
+++ install.sh
@@ -39,9 +39,3 @@ antigen update
 
 
 
-# vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-pip install -U pynvim
-nvim +'PlugInstall --sync' +qa
-nvim +'LspInstall vimls pylsp jsonls rust_analyzer'
