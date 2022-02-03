69a7c9caa031a82bf28f748fb96681be7b6805c5
Charles Christensen <charles.n.chr@gmail.com>
2022-02-03 00:43:34 +0000

    Arch install packages
    Former-commit-id: a76319c21362a2a07ca185985aa148ddb69bb758

---
+++ install_arch.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syu yay
+
+yay --needed --noconfirm -S firefox neovim zsh tmux inkscape libinput-gestures maim teams discord zoom nerd-fonts-source-code-pro wmctrl xdotool rofi rofimoji alttab-git tmuxinator spotify google-chrome code texlive-core dolphin dolphin-plugins ark cheese betterlockscreen py3status playerctl yarn redshift kcolorchooser darktable konsole kolourpaint pacdep discover flatpak packagekit-qt5 ttf-font-awesome dust mpd rofi-calc imagej rclone synology-drive firefox-pwa stow handlr-bin tig i3status-rust noto-fonts-emoji ktorrent solaar rofi-greenclip zathura
+
+sudo pacman -S texlive-most
