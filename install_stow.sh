830793c9391b29c0a257eeb635c97b1bd73ad3aa
Charles Christensen <charles.n.chr@gmail.com>
2022-02-09 11:02:05 +0000

    Stow select menus
    Former-commit-id: 54b9ad86069774a7ae561c89cea8a740589f9d73

--- install_stow.sh
+++ install_stow.sh
@@ -3,28 +3,54 @@
 # alternative, less clean, syntax:
 # stow -R .config -t ~/.config
 
-echo "Delete existing .profile, .zshrc, dunstrc, mimeapps.list?"
+echo "Move existing .profile, .zshrc, dunstrc, mimeapps.list, nvim to a folder named conf_bk?"
 select yn in "Yes" "No"; do
   case $yn in
     Yes )
-        rm ~/.profile
-        rm ~/.zshrc
-        rm -rf ~/.config/dunst
-        rm ~/.config/mimeapps.list
+        mkdir -p conf_bk
+        mv ~/.profile conf_bk
+        mv ~/.zshrc conf_bk
+        mv ~/.config/dunst conf_bk
+        mv ~/.config/mimeapps.list conf_bk
+        mv ~/.config/libinput-gestures.conf conf_bk
+        mv ~/.config/nvim conf_bk
+        mv ~/.config/rofi conf_bk
+        mv ~/.config/alacritty conf_bk
+        mv ~/bin/i3exit conf_bk
+        break
         ;;
-    No ) exit;;
+    No ) break;; #exit;;
   esac
 done
 
 
 stow home_folder
-stow home_folder_desktop
-
-stow nvim
-stow ricing
-
-stow i3
-
 stow scripts
+stow nvim
+
+
+echo "Select extra config"
+select yn in "Desktop" "Laptop" "Server" "None"; do
+  case $yn in
+    Desktop )
+        stow home_folder_desktop
+        break
+        ;;
+    Laptop )
+        stow home_folder_laptop
+        break
+        ;;
+    Server )
+        exit
+        ;;
+    None )
+        break
+        ;;
+  esac
+done
+
+
+stow ricing
+stow i3
 stow alacritty
 stow mime-settings
