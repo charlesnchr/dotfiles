3bd914eb96892c0320d1af9a79317a4ce42f650a
Charles Christensen <charles.n.chr@gmail.com>
2022-03-02 23:01:21 +0000

    Fixes

--- install_private.sh
+++ install_private.sh
@@ -4,5 +4,6 @@ git config --global user.name "Charles Christensen"
 git config --global user.email "charles.n.chr@gmail.com"
 
 echo 'setup private'
-git submodule add git@github.com:charlesnchr/dotfiles_private
+# git submodule add git@github.com:charlesnchr/dotfiles_private
+git submodule init dotfiles_private
 stow dotfiles_private
