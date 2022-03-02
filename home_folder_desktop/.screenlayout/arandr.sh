e17d29afa2134ca7aa6d832a14766f1eb3490311
Charles Christensen <charles.n.chr@gmail.com>
2022-03-02 14:42:54 +0000

    Updated for nvidia gpu main desktop

--- home_folder_desktop/.screenlayout/arandr.sh
+++ home_folder_desktop/.screenlayout/arandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --set TearFree on --mode 2560x1440 --pos 5120x0 --rotate normal --output DisplayPort-1 --set TearFree on --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off --output DisplayPort-3 --primary --set TearFree on --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-4 --off
+xrandr --output DP-2.8 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-5 --off
