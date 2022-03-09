1f7b4d2c58a05640e2877a5b271c896e14831e15
Charles Christensen <charles.n.chr@gmail.com>
2022-03-09 14:01:44 +0000

    Small update

---
+++ home_folder_desktop/.screenlayout/arandr_tv_60.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2.8 --off --output HDMI-0 --primary --rate 60 --mode 3840x2160 --pos 1814x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
