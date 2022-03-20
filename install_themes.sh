de21d0c748ae878b4938579e83e32815f2bc4433
Charles Christensen <charles.n.chr@gmail.com>
2022-03-20 17:30:51 +0000

    Small update

--- install_themes.sh
+++ install_themes.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+source ./ask.sh
+
 # KDE theme
-git clone https://github.com/tonyfettes/materia-nord-kvantum.git
-cd materia-nord-kvantum
-sudo mv Kvantum/MateriaNordDark /usr/share/Kvantum
+if ask "Install kde theme"; then
+    git clone https://github.com/tonyfettes/materia-nord-kvantum.git
+    cd materia-nord-kvantum
+    sudo mv Kvantum/MateriaNordDark /usr/share/Kvantum
+fi
 
 # GTK theme
 # https://github.com/EliverLara/Nordic
 # https://www.gnome-look.org/p/1267246/
-wget https://github.com/EliverLara/Nordic/releases/download/v2.1.0/Nordic-darker-v40.tar.xz
-tar xvf Nordic-darker-v40.tar.xz
-sudo mv Nordic-darker-v40 /usr/share/themes
-gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
-gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+if ask "Install GTK theme"; then
+    wget https://github.com/EliverLara/Nordic/releases/download/v2.1.0/Nordic-darker-v40.tar.xz
+    tar xvf Nordic-darker-v40.tar.xz
+    sudo mv Nordic-darker-v40 /usr/share/themes
+    gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+    gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+fi
