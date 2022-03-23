58fd42009a8754c6950a08e93d0d5a7d3cda7445
Charles Christensen <charles.n.chr@gmail.com>
2022-03-23 22:40:02 +0000

    Xidlehook for screenlocking respecting xfce presentation mode

--- scripts/bin/i3status-inactivity-suspend.sh
+++ scripts/bin/i3status-inactivity-suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 : ' ----------------------------------------
 * Creation Time : Wed 23 Mar 2022 10:55:35 GMT
-* Last Modified : Wed 23 Mar 2022 16:31:54 GMT
+* Last Modified : Wed 23 Mar 2022 21:41:29 GMT
 * Author : Charles N. Christensen
 * Github : github.com/charlesnchr
 ----------------------------------------'
@@ -11,7 +11,7 @@ CURSETTING=$(xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inacti
 
 if [ "$CMD" = "set" ]; then
     if [ "$CURSETTING" = 14 ]; then
-        xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac" -t int -s 1
+        xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac" -t int -s 20
     else
         xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac" -t int -s 14
     fi
