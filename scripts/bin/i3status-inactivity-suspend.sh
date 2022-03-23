c09c085bb544f726744092142dfed10fa77ddac0
Charles Christensen <charles.n.chr@gmail.com>
2022-03-23 11:51:54 +0000

    For xfce power management through statusbar

---
+++ scripts/bin/i3status-inactivity-suspend.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+: ' ----------------------------------------
+* Creation Time : Wed 23 Mar 2022 10:55:35 GMT
+* Last Modified : Wed 23 Mar 2022 11:32:22 GMT
+* Author : Charles N. Christensen
+* Github : github.com/charlesnchr
+----------------------------------------'
+
+CMD=$1
+CURSETTING=$(xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac")
+
+if [ "$CMD" = "set" ]; then
+    if [ "$CURSETTING" = 14 ]; then
+        xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac" -s 20
+    else
+        xfconf-query -c xfce4-power-manager -p "/xfce4-power-manager/inactivity-on-ac" -s 14
+    fi
+else # get
+    if [ "$CURSETTING" = 14 ]; then
+        echo ""
+    else
+        echo ""
+    fi
+fi
