bdd783e456c340eea7ebb5f7bef8f9007572b42c
Charles Christensen <charles.n.chr@gmail.com>
2022-04-16 22:58:21 +0100

    Added ncspot

--- home_folder/.config/awesome/auto-start-always.sh
+++ home_folder/.config/awesome/auto-start-always.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 : ' ----------------------------------------
 * Creation Time : Mon 11 Apr 2022 22:00:29 BST
-* Last Modified : Sat 16 Apr 2022 05:22:37 BST
+* Last Modified : Sat 16 Apr 2022 22:43:29 BST
 * Author : Charles N. Christensen
 * Github : github.com/charlesnchr
 ----------------------------------------'
@@ -27,3 +27,4 @@ ppgrep python | grep usbwatchdog.py || ~/anaconda3/bin/python ~/bin/usbwatchdog.
 # find pwa app
 # exo-open $(rg -i spotify /home/cc/.local/share/applications | awk '{split($0,a,":"); print a[1]}')
 # pgrep Spotify || exo-open ~/.local/share/applications/webcatalog-spotify.desktop
+pgrep ncspot || kitty -e ~/bin/ncspot
