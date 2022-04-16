16685fdf187f6c10356421ce6ed91d24dc098167
Charles Christensen <charles.n.chr@gmail.com>
2022-04-16 23:40:08 +0100

    Clean up

--- home_folder/.config/awesome/volume-widget/volume.lua
+++ home_folder/.config/awesome/volume-widget/volume.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------
 -- The Ultimate Volume Widget for Awesome Window Manager
 -- More details could be found here:
--- https://github.com/streetturtle/awesome-wm-widgets/tree/master/volume-widget
+-- https://github.com/streetturtle//tree/master/volume-widget
 
 -- @author Pavel Makhov
 -- @copyright 2020 Pavel Makhov
@@ -13,7 +13,7 @@ local spawn = require("awful.spawn")
 local gears = require("gears")
 local beautiful = require("beautiful")
 local watch = require("awful.widget.watch")
-local utils = require("awesome-wm-widgets.volume-widget.utils")
+local utils = require("volume-widget.utils")
 
 
 local LIST_DEVICES_CMD = [[sh -c "pacmd list-sinks; pacmd list-sources"]]
@@ -24,11 +24,11 @@ local function TOG_VOLUME_CMD(device) return 'amixer -D ' .. device .. ' sset Ma
 
 
 local widget_types = {
-    icon_and_text = require("awesome-wm-widgets.volume-widget.widgets.icon-and-text-widget"),
-    icon = require("awesome-wm-widgets.volume-widget.widgets.icon-widget"),
-    arc = require("awesome-wm-widgets.volume-widget.widgets.arc-widget"),
-    horizontal_bar = require("awesome-wm-widgets.volume-widget.widgets.horizontal-bar-widget"),
-    vertical_bar = require("awesome-wm-widgets.volume-widget.widgets.vertical-bar-widget")
+    icon_and_text = require("volume-widget.widgets.icon-and-text-widget"),
+    icon = require("volume-widget.widgets.icon-widget"),
+    arc = require("volume-widget.widgets.arc-widget"),
+    horizontal_bar = require("volume-widget.widgets.horizontal-bar-widget"),
+    vertical_bar = require("volume-widget.widgets.vertical-bar-widget")
 }
 local volume = {}
 
