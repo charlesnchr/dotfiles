c44dda8f84b12c10868edd7b1de31a2bc69c9558
Charles <cc@Charless-MacBook-Pro.local>
2022-05-02 21:07:19 +0100

    Macos config with yabai

---
+++ home_folder/.config/yabai/open_firefox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+: ' ----------------------------------------
+* Creation Time : Fri Apr 29 14:29:37 2022
+* Last Modified : Sun May  1 20:15:39 2022
+* Author : Charles N. Christensen
+* Github : github.com/charlesnchr
+----------------------------------------'
+
+open -na /Applications/Firefox.app
+
+
