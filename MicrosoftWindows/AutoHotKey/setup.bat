051b9df7aa48ebc7aac0d68f185dca56172afefe
Charles N. Christensen <charles.n.chr@gmail.com>
2022-08-28 10:56:03 +0100

    Added Autohotkey

---
+++ MicrosoftWindows/AutoHotKey/setup.bat
@@ -0,0 +1,8 @@
+cd %USERPROFILE%\Documents
+mklink AutoHotKey.ahk "C:\Users\charl\ccdrive\3.Diverse"\IT\AutoHotKey\AutoHotKey.ahk
+mklink GUI "C:\Users\charl\ccdrive\3.Diverse"\IT\AutoHotKey\GUI
+mklink Miscellaneous "C:\Users\charl\ccdrive\3.Diverse"\IT\AutoHotKey\Miscellaneous
+@REM mklink AutoHotKey.ahk %USERPROFILE%\"Dropbox"\"3.Diverse"\IT\AutoHotKey\AutoHotKey.ahk
+@REM mklink GUI %USERPROFILE%\"Dropbox"\"3.Diverse"\IT\AutoHotKey\GUI
+@REM mklink Miscellaneous %USERPROFILE%\"Dropbox"\"3.Diverse"\IT\AutoHotKey\Miscellaneous
+pause
