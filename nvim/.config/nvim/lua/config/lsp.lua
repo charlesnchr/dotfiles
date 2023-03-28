79f717f48c7353a53b2f1889f4e9d55478af3e01
Charles N. Christensen <charles.n.chr@gmail.com>
2023-03-28 13:40:02 +0100

    Disable var highlighting

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -103,16 +103,16 @@ local function config(_config)
 			vim.keymap.set("n", "<space>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
 			-- The blow command will highlight the current variable and its usages in the buffer.
-			vim.cmd([[
-      hi! link LspReferenceRead Visual
-      hi! link LspReferenceText Visual
-      hi! link LspReferenceWrite Visual
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]])
+			-- vim.cmd([[
+      -- hi! link LspReferenceRead Visual
+      -- hi! link LspReferenceText Visual
+      -- hi! link LspReferenceWrite Visual
+      -- augroup lsp_document_highlight
+        -- autocmd! * <buffer>
+        -- autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        -- autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      -- augroup END
+    -- ]])
 
 			if vim.g.logging_level == "debug" then
 				local msg = string.format("Language server %s started!", client.name)
