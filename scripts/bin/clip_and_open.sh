d5f6a3d4ee72257239cfb078b232afc4cab8cf1e
Charles Christensen <charles.n.chr@gmail.com>
2023-08-28 20:08:58 +0100

    Three new scripts

---
+++ scripts/bin/clip_and_open.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+: ' ----------------------------------------
+* Creation Time : Sat 26 Aug 2023 20:51:09 BST
+* Author : Charles N. Christensen
+* Github : github.com/charlesnchr
+----------------------------------------'
+
+head -n -1 | nvim -
