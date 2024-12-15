99d0305304f1f4380734af75a1918c839e330d8b
Charles N. Christensen <charles.n.chr@gmail.com>
2024-12-15 03:30:19 +0000

    Remove copilot chat

--- nvim/.config/nvim/lua/lua-init.lua
+++ nvim/.config/nvim/lua/lua-init.lua
@@ -408,72 +408,74 @@ end
 -- }
 
 
-require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    hide_during_completion = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
+-- require('copilot').setup({
+--   panel = {
+--     enabled = true,
+--     auto_refresh = false,
+--     keymap = {
+--       jump_prev = "[[",
+--       jump_next = "]]",
+--       accept = "<CR>",
+--       refresh = "gr",
+--       open = "<M-CR>"
+--     },
+--     layout = {
+--       position = "bottom", -- | top | left | right
+--       ratio = 0.4
+--     },
+--   },
+--   suggestion = {
+--     enabled = true,
+--     auto_trigger = true,
+--     hide_during_completion = true,
+--     debounce = 75,
+--     keymap = {
+--       accept = "<M-l>",
+--       accept_word = false,
+--       accept_line = false,
+--       next = "<M-]>",
+--       prev = "<M-[>",
+--       dismiss = "<C-]>",
+--     },
+--   },
+--   filetypes = {
+--     yaml = false,
+--     markdown = false,
+--     help = false,
+--     gitcommit = false,
+--     gitrebase = false,
+--     hgcommit = false,
+--     svn = false,
+--     cvs = false,
+--     ["."] = false,
+--   },
+--   copilot_node_command = 'node', -- Node.js version must be > 18.x
+--   server_opts_overrides = {},
+-- })
+
+-- require("CopilotChat").setup {
+--   debug = true, -- Enable debugging
+--   -- See Configuration section for rest
+-- }
+
+-- -- deps:
+-- require('img-clip').setup ({
+--   -- use recommended settings from above
+-- })
+-- require('avante_lib').load()
+-- require('avante').setup ({
+--   provider = "openai",
+--   auto_suggestions_provider = "copilot",
+--   mappings = {
+--       ask = "<space>ua", -- ask
+--       edit = "<space>ue", -- edit
+--       refresh = "<space>ur", -- refresh
+--   },
+-- })
+
+require("smartcolumn").setup({
+ scope = "line",
 })
-
-require("CopilotChat").setup {
-  debug = true, -- Enable debugging
-  -- See Configuration section for rest
-}
-
--- deps:
-require('img-clip').setup ({
-  -- use recommended settings from above
-})
-require('avante_lib').load()
-require('avante').setup ({
-  provider = "openai",
-  auto_suggestions_provider = "copilot",
-  mappings = {
-      ask = "<space>ua", -- ask
-      edit = "<space>ue", -- edit
-      refresh = "<space>ur", -- refresh
-  },
-})
-
-
 
 return M
 
