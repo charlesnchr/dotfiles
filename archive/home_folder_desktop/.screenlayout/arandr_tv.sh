ff89f3c9ce9b13f86eade52e2997d4f6c3bdbf31
Charles N. Christensen <charles.n.chr@gmail.com>
2025-08-24 17:57:05 +0100

    Delete old files

--- archive/home_folder_desktop/.screenlayout/arandr_tv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-2.8 --off --output HDMI-0 --primary --rate 119.88 --mode 3840x2160 --pos 1814x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
--- archive/install_lockscreen.sh
+++
@@ -1,1 +0,0 @@
-betterlockscreen -u ~/dotfiles/wallpapers/yosemite_winter_photography-wallpaper-2560x1440.jpg --blur 0.8
