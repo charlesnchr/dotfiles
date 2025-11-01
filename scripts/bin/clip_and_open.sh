f4a52b92b19c00432edff9faaa7c71376b4a9add
Charles N. Christensen <charles.n.christensen@gsk.com>
2025-11-01 12:27:13 +0000

    chore: clean up no longer used files

--- scripts/bin/clip_and_open.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-: ' ----------------------------------------
-* Creation Time : Sat 26 Aug 2023 20:51:09 BST
-* Author : Charles N. Christensen
-* Github : github.com/charlesnchr
-----------------------------------------'
-
-head -n -1 | nvim -
